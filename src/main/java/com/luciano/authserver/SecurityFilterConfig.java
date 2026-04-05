66c0409ecf213f698c39faa1c5cc91228c64d86b
kelu241 <kelu241@gmail.com>
2026-04-05 16:13:43 -0300

    feat:primeira camada

--- src/main/java/com/luciano/authserver/SecurityFilterConfig.java
+++ src/main/java/com/luciano/authserver/SecurityFilterConfig.java
@@ -3,22 +3,40 @@ package com.luciano.authserver;
 import org.springframework.security.config.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 public class SecurityFilterConfig {
-
+  @Order(1)
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    http.oauth2AuthorizationServer(oauth2 -> oauth2.oidc(Customizer.withDefaults()));
+    OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+    http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults());
 
-    http.exceptionHandling(
-        exceptons -> exceptons.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")));
+    http.oauth2ResourceServer(new Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>>() {
+      @Override
+      public void customize(OAuth2ResourceServerConfigurer<HttpSecurity> conf) {
 
-    http.oauth2ResourceServer(conf -> conf.jwt(Customizer.withDefaults()));
+        conf.jwt(Customizer.withDefaults());
+      }
+    });
+
+    http.exceptionHandling(new Customizer<ExceptionHandlingConfigurer<HttpSecurity>>() {
+      @Override
+      public void customize(ExceptionHandlingConfigurer<HttpSecurity> exceptionHandling) {
+
+        exceptionHandling.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
+
+      }
+    });
 
     return http.build();
 
